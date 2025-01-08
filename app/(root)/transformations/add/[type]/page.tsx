6fb2bae2464b11d5d648e3dce5b295e4c3069878
blue4e2 <blue.4e2@gmail.com>
2025-01-08 14:39:55 +0530

    webhook added

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import Header from "@/components/shared/Header"
+import TransformationFrom from "@/components/shared/TransformationFrom";
+import { transformationTypes } from "@/constants"
+import { getUserById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = async({ params }: SearchParamProps) => {
+  const { userId } = await auth();
+  const { type } = await params;
+  const transformation = transformationTypes[type];
+
+  if(!userId){
+    redirect("/sign-in")
+  }else{
+    console.log(userId)
+  }
+
+  const user = await getUserById(userId);
+
+  if(!user) console.log("error caught")
+
   return (
-    <div>AddTransformationTypePage</div>
+    <>
+      <Header 
+        title={transformation.title}
+        subtitle={transformation.subTitle}
+      />
+      <TransformationFrom
+        action="Add"
+        userId={user._id}
+        type={transformation.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+      />
+    </>
   )
 }
 
